e3864201d4880455bf938fd99a6d80a03a27f7b8
zhaokai <820641374@qq.com>
2018-06-17 09:30:20 +0800

    优化服务器

--- server/blog/module/article_httphandler.js
+++ server/blog/module/article_httphandler.js
@@ -2,29 +2,44 @@
 数据的增删改查以及其他操作
 */
 var db = require("../database/db.js")
-var article_schema= require('../database/article.js')
+var article_schema = require('../database/article.js')
 var articleModel = db.model('article', article_schema)
-
-var queryList = function(req,res,next){
-        articleModel.find({},function(error,docs){
-        	console.log('test')
-        	res.send(docs);
-        })
+//查询所有数据
+var queryList = function (req, res, next) {
+    articleModel.find({}, function (error, docs) {
+        res.json(docs)
+    })
+}
+//查询单个文章数据
+var queryArticle = function (req, res, next) {
+    let id = req.params.id
+    articleModel.find({_id: id}, function (error, docs) {
+        res.json(docs)
+    })
+    res.json({data: {}})
+}
+//修改单个文章数据
+var updateArticle = function (req, res, next) {
+    let id = req.params.id
 
 }
-var addList = function(req,res,next){
-	var mongooseEntity = new articleModel({});
-        mongooseEntity.save(function(error) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('saved OK!');
-            }
-        });
-        console.log(JSON.stringify(req))
-        res.send('iii')
+//增加数据
+var addList = function (req, res, next) {
+    var mongooseEntity = new articleModel(req.query);
+    mongooseEntity.save(function (error) {
+        if (error) {
+            res.send(error)
+            console.log(error)
+
+        } else {
+            res.send('saved OK!')
+        }
+    });
 }
+
 module.exports = {
-	queryList: queryList,
-	addList: addList
+    queryList: queryList,
+    addList: addList,
+    queryArticle: queryArticle,
+    updateArticle: updateArticle
 }
--- server/blog/routes/index.js
+++ server/blog/routes/index.js
@@ -3,13 +3,26 @@ var router = express.Router();
 var articleHttpHandler = require('../module/article_httphandler')
 
 /* GET home page. */
+router.route('/admin')
+    .get(function (req,res,next) {
+        res.render('index.ejs', { title: 'CSSer, 关注Web前端技术！' });
+    })
+
+//管理文章信息
 router.route('/article/list')
     .get(function(req, res, next) {
         articleHttpHandler.queryList(req, res, next)
     })
-    .put(function(req, res,next) {
+    .put(function(req, res, next) {
         articleHttpHandler.addList(req, res, next)
         res.send('add data')
     })
+router.route('/article/detail/:id')
+    .get(function(req, res, next) {
+    	articleHttpHandler.queryArticle(req,res,next)
 
+    })
+    .post(function(req, res, next) {
+
+    })
 module.exports = router;
