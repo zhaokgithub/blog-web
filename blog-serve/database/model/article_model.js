202cbf172518d3104a88493452b788d784d65f8e
zhaokai <820641374@qq.com>
2020-02-17 10:46:56 +0800

    update

--- blog-serve/database/model/article_model.js
+++ blog-serve/database/model/article_model.js
@@ -3,3 +3,41 @@
  * @author zhaokai
  * @date 2019/10/27
  */
+let mongoose = require('../db.js')
+
+let Schema = mongoose.Schema
+let ArticleSchema = new Schema({
+
+    collectionCount: { type: Number },
+    userRankIndex: { type: Number },
+    buildTime: { type: Number },
+    commentsCount: { type: Number },
+    gfw: { type: Boolean },
+    objectId: { type: String },
+    checkStatus: { type: Boolean },
+    isEvent: { type: Boolean },
+    entryView: { type: String },
+    subscribersCount: { type: Number },
+    ngxCachedTime: { type: Number },
+    verifyStatus: { type: Boolean },
+    // tags: [{ngxCachedTime: {type:Number}, ngxCached: {type:Boolean},, title: "命令行", id: "5597a2cee4b08a686ce5b7e6",…}],
+    updatedAt: { type: Date },
+    rankIndex: { type: Number },
+    hot: { type: Boolean },
+    autoPass: { type: Boolean },
+    originalUrl: { type: String },
+    verifyCreatedAt: { type: Date },
+    createdAt: { type: Date },
+    user: { type: String },
+    author: { type: String },
+    screenshot: { type: String },
+    original: { type: Boolean },
+    hotIndex: { type: Number },
+    content: { type: String },
+    title: { type: String },
+    lastCommentTime: { type: Date },
+    type: { type: String },
+    english: { type: Boolean },
+    // category: {ngxCached: {type:Boolean},, title: "backend", id: "5562b419e4b00c57d9b94ae2", name: "后端",…},
+    viewsCount: { type: Number },
+})
--- blog-serve/database/model/user_model.js
+++ blog-serve/database/model/user_model.js
@@ -5,7 +5,7 @@
  */
 let mongoose = require('../db');
 
-Schema = mongoose.Schema;
+let Schema = mongoose.Schema;
 let UserSchema = new Schema({
     user_name : { type: String }, //用户账号
     password: {type: String}, //密码
@@ -13,7 +13,17 @@ let UserSchema = new Schema({
     email:{type:String},//邮箱
     head:{type:String},//头像
     role:{type:Number},//角色
-    create_time : { type: Date} //最近登录时间
+    create_time : { type: Date}, //用户创建时间
+    author:{type:Number},//authority
+    company:{type:String},
+    memo:{type:String},
+    repository:{type:String},
+    article_count:{type:Number},
+    followers_count:{type:Number},
+    followees_count:{type:Number},
+    total_comments_count:{type:Number},
+    total_collections_count:{type:Number},
+    posted_posts_count:{type:Number},
 });
 
 module.exports = mongoose.model('user',UserSchema);
